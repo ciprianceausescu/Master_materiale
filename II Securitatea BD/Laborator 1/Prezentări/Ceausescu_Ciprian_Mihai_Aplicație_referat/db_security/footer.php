58c95573b9992ca843ed2857d195b97390c80fb4
ciprianceausescu <ciprian-mihai.ceausescu@my.fmi.unibuc.ro>
2018-12-04 21:21:06 +0200

    Prezentari

---
+++ II Securitatea BD/Laborator 1/Prezentări/Ceausescu_Ciprian_Mihai_Aplicație_referat/db_security/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+  <p class="text-center">Teacher: Lect. Dr. Letiția Marin</p>
+  <p class="text-center">Student: Ciprian Mihai - Ceaușescu</p>
+  <p class="text-center"><a href="http://fmi.unibuc.ro/ro" target="_blank">Faculty of Mathematics and Computer Science</a> - Software Engineering</p>
+</div>
